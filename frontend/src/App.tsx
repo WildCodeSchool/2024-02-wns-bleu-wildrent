96d3e80f8bb41e0bfcdd24054823a8523de99b5b
dev3Socializus <dev3@socializus.org>
2024-07-11 10:49:59 +0200

    feature: resolve conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/Home";
-import NewProduct from "./pages/NewProduct";
-import NewArticle from "./pages/NewArticle";
 import Layout from "./components/Layout";
 import Register from "./pages/Register";
 import ProductDescription from "./pages/ProductDescription";
@@ -14,8 +12,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="admin" element={<NewProduct />} />
-        <Route path="admin/new-article" element={<NewArticle />} />
         <Route path="register" element={<Register />} />
         <Route path="admin" element={<Admin />} />
         <Route path="login" element={<Login />} />
