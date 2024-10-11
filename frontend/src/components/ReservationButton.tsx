3ce26a988b89cfb152cc5dbbb0e6359147aafce8
Hafsa <hafsa.aitouarraou@outlook.fr>
2024-10-11 11:45:25 +0200

    feature/message/103: added an error message if user is not connected and trying to reserve an article

--- frontend/src/components/ReservationButton.tsx
+++ frontend/src/components/ReservationButton.tsx
@@ -1,12 +1,15 @@
 import { Button, message } from "antd";
 import { Article } from "../interface/types";
 import { useHandleReservationMutation } from "../generated/graphql-types";
+import { useContext, useEffect } from "react";
+import { UserContext } from "./Layout";
 
 export default function ReservationButton({
   articles,
 }: {
   articles: Article[];
 }) {
+  const userInfo = useContext(UserContext);
   const [handleReservation] = useHandleReservationMutation({
     onCompleted(data) {
       message.success("Réservation effectuée avec succès");
@@ -22,12 +25,18 @@ export default function ReservationButton({
       message.error("Erreur lors de la réservation");
     },
   });
-
+  useEffect(() => {
+    console.log(userInfo.isLoggedIn, "userInfo");
+  });
   const onClick = () => {
     const startDateString = localStorage.getItem("startDate");
     const startDate = startDateString ? new Date(startDateString) : null;
     const endDateString = localStorage.getItem("endDate");
     const endDate = endDateString ? new Date(endDateString) : null;
+
+    if (userInfo.isLoggedIn === false) {
+      message.error("Veuillez vous connecter pour pouvoir réserver un article");
+    }
 
     if (!startDate || !endDate) {
       message.error("Choisissez une période de location");
@@ -38,11 +47,10 @@ export default function ReservationButton({
     const availableArticles = articles.filter(
       (article) => article.availability === true
     );
-    if (availableArticles.length === 0) {
+    if (availableArticles.length === 0 && userInfo.isLoggedIn === true) {
       message.error("Cet article n'est pas disponible");
       return;
     }
-
 
     // TO BE DISCUSSED : comment choisir l'article si plusieurs sont dispo ?
     const firstAvailableArticleId = availableArticles[0].id;
